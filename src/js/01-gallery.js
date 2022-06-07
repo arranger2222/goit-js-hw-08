78cc16aed1eab4a3912e0d93eef8feeb25c8f4c1
Max Yuvkovetskiy <yuvkovetskyy.maxim@gmail.com>
2022-06-07 20:22:46 +0300

    added gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// console.log(galleryItems);
+
+const refs = {
+    gallery: document.querySelector('.gallery'),
+};
+
+refs.gallery.innerHTML = createGalleryMarkup();
+
+const createdGallery = new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionType: 'attr',
+    overlay: true,
+    captionsData: 'alt',
+});
+
+function createGalleryMarkup(){
+    return galleryItems.map(({ preview, original, description }) => 
+        `<a class="gallery__item" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}" />
+                    </a>`)
+        .join('')
+};
 
 console.log(galleryItems);
+
