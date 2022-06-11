9e9baab1cb7d6315f32f352162f94695609d12af
Max Yuvkovetskiy <yuvkovetskyy.maxim@gmail.com>
2022-06-11 19:12:59 +0300

    finished feedback form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,55 +2,39 @@ import throttle from "lodash.throttle";
 
 const STORAGE_KEY = "feedback-form-state";
 const form = document.querySelector('.feedback-form');
-const email = form.email;
-const textArea = form.message;
+let email = form.email;
+let textArea = form.message;
+const formData = {};
 
 
 form.addEventListener('submit', onFormSubmit);
 email.addEventListener('input', throttle(onFormInput, 500));
 textArea.addEventListener('input', throttle(onFormInput, 500));
 
+getDataFromStorage();
+
+
 function onFormSubmit(evt) {
     evt.preventDefault();  
-    console.log('Отправляем форму');
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
-  };
-
-  function onFormInput(evt) {  
-      
-    formData[evt.target.name] = evt.target.value;
-    const dataToStorage = JSON.stringify('[formData]');
-    console.log(dataToStorage);
-    localStorage.setItem(STORAGE_KEY, dataToStorage);
+    console.log(formData);
   };
 
 
+function onFormInput(evt) {    
+    formData[evt.target.name] = evt.target.value;    
+    const dataToStorage = JSON.stringify(formData);
+    localStorage.setItem(STORAGE_KEY, dataToStorage);    
+  };
 
 
-
-
-
-//const {
-//     elements: { login, password }
-//   } = event.currentTarget;
-
-
-
-// const form = document.querySelector('.login-form');
-
-// form.addEventListener('submit', handleSubmit);
-
-// function handleSubmit(event){
-//     event.preventDefault();
-//     const { 
-//     elements: { email, password }  
-//     } = event.currentTarget;
-//    if(email.value === '' || password.value === ''){
-//     alert('Please fill in all the required fields. ')
-// }
-// const userFormData = {email: email.value, password: password.value};
-// console.log(userFormData);
-// event.currentTarget.reset();
-// };
-
+function getDataFromStorage() {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    const parsedData = JSON.parse(savedData);
+  
+    if (savedData) {
+      email.value = parsedData.email;
+      textArea.value = parsedData.message;
+    }  
+};
